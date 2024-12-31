4c882e8fc00c30359056c62aeb9d8c07e86f3252
Cess172 <vpiololomod@icloud.com>
2024-12-31 16:39:12 +0800

    Update Bot.ino

--- Bot.ino
+++ Bot.ino
@@ -1,13 +1,16 @@
 #include <SoftwareSerial.h>
 
-#define RELAY_PIN_1 7
+#define RELAY_PIN_1 7  // Relay for the initial functionality
+#define RELAY_PIN_2 4  // Relay for lights
 
 void setup() {
-  // Set relay pin as output
+  // Set relay pins as output
   pinMode(RELAY_PIN_1, OUTPUT);
+  pinMode(RELAY_PIN_2, OUTPUT);
 
-  // Ensure relay is initially OFF
+  // Ensure relays are initially OFF
   digitalWrite(RELAY_PIN_1, LOW);
+  digitalWrite(RELAY_PIN_2, LOW);
 
   // Start serial communication
   Serial.begin(9600);
@@ -19,13 +22,24 @@ void loop() {
     String command = Serial.readStringUntil('\n');
 
     if (command == "engage") {
-      // Activate the relay
+      // Activate the first relay
       digitalWrite(RELAY_PIN_1, HIGH);
       Serial.println("Relay engaged");  // Send feedback
-      delay(1700);                      // Keep relay on for 2.5 seconds
+      delay(1700);                      // Keep relay on for 1.7 seconds
       digitalWrite(RELAY_PIN_1, LOW);   // Turn off relay
       Serial.println("Relay disengaged");
-    } else {
+    } 
+    else if (command == "lights") {
+      // Turn on lights relay
+      digitalWrite(RELAY_PIN_2, HIGH);
+      Serial.println("Lights turned on");
+    } 
+    else if (command == "lightoff") {
+      // Turn off lights relay
+      digitalWrite(RELAY_PIN_2, LOW);
+      Serial.println("Lights turned off");
+    } 
+    else {
       Serial.println("Unknown command");
     }
   }
